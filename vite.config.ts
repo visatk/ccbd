2af0ab5f255fc3f2b0cc6b061bdc08849ef25f91
visatk <visatk29@gmail.com>
2026-04-30 18:10:21 +0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    cloudflare(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
